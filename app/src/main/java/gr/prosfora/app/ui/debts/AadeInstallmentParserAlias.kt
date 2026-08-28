9aecce4aaa7277f3080709b452fccb7a43fb730b
scanmydata <scanmydata@hotmail.com>
2026-08-28 11:45:43 +0300

    fix: expose AADE installment parser to debt UI package

---
+++ app/src/main/java/gr/prosfora/app/ui/debts/AadeInstallmentParserAlias.kt
@@ -0,0 +1,4 @@
+package gr.prosfora.app.ui.debts
+
+/** Convenience alias for the AADE installment parser used by the import dialog. */
+typealias AadeInstallmentParser = gr.prosfora.app.debt.AadeInstallmentParser
