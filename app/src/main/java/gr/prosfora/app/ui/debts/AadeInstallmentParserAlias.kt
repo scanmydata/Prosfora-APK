f7082a129d8085e932f3f6f7344cd42ecc1d6c46
scanmydata <scanmydata@hotmail.com>
2026-08-28 11:48:19 +0300

    fix: replace invalid AADE parser typealias

--- app/src/main/java/gr/prosfora/app/ui/debts/AadeInstallmentParserAlias.kt
+++
@@ -1,4 +0,0 @@
-package gr.prosfora.app.ui.debts
-
-/** Convenience alias for the AADE installment parser used by the import dialog. */
-typealias AadeInstallmentParser = gr.prosfora.app.debt.AadeInstallmentParser
