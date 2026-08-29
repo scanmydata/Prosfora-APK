4b594753325041b0980425820c7f4fbe526f22fc
scanmydata <scanmydata@hotmail.com>
2026-08-29 03:17:19 +0300

    Fix OffersListScreen Card import

--- app/src/main/java/gr/prosfora/app/ui/offers/OffersListScreen.kt
+++ app/src/main/java/gr/prosfora/app/ui/offers/OffersListScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Send
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Sms
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
